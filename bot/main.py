e7ff20ac68254c424f5bc52d64922462d3836af1
artem3605 <artem3605@bk.ru>
2021-12-22 10:01:56 +0300

    Merge branch 'main' of C:\Users\Artem\Documents\GitHub\CGI-tg-bot with conflicts.

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,4 @@
-import os
+import asyncio
 import queue
 import urllib
 
@@ -6,9 +6,14 @@ from aiogram import Bot
 from aiogram import types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import BOT_TOKEN
+import os
+import sys
 
+sys.path.append('../')
+sys.path.append('Stylegan3')
+
+from config import BOT_TOKEN
+from wokrer import RecognizeThread, send_ready_images
 
 bot = Bot(token=BOT_TOKEN)
 
@@ -27,29 +32,23 @@ async def some_handler(message: types.Message):
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
     document_id = message['photo'][-1].file_id
-    file_id = message['photo'][-1].file_unique_id
 
     file_info = await bot.get_file(document_id)
     filename, file_extension = os.path.splitext(file_info.file_path)
-    image_name = os.path.join('photos', file_id + file_extension)
+    image_name = os.path.join('photos', document_id + file_extension)
     urllib.request.urlretrieve(f'https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}', image_name)
     task_queue.put((image_name, message.chat.id))
-
-    markup = InlineKeyboardMarkup()
-    markup.row_width = 2
-    markup.add(InlineKeyboardButton(":thumbs_up:", callback_data="yes"),
-               InlineKeyboardButton(":thumbs_down:", callback_data=f"no_{image_name}"))
-
-    with open(image_name, 'rb') as image:
-        await bot.send_photo(chat_id=message['from']['id'], photo=image, reply_markup=markup)
-
-
-@dp.callback_query_handler(lambda x: x.data.startswith('no'))
-async def foo(call: types.CallbackQuery):
-    os.remove(call.data[3:])
+    await message.answer("Ваше изображение обрабатывается, это займет примерно {} минут \n".format(recognize_thread.get_waiting_time()))
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    recognize_thread = RecognizeThread(task_queue)
+    recognize_thread.daemon = True
+    recognize_thread.start()
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(send_ready_images(recognize_thread.ready_queue, bot))
+
+    executor.start_polling(dp, loop=loop)
 
 
--- bot/wokrer.py
+++ bot/wokrer.py
@@ -1,22 +1,18 @@
-import asyncio
 import queue
-import sys
+import time
 from threading import Thread
+import asyncio
 
 import cv2
+import numpy as np
+from PIL import Image
+from facenet_pytorch import MTCNN
+import torch
+
 import dnnlib
 import legacy
-import numpy as np
 import predict
-import torch
 from metrics.metric_utils import get_feature_detector
-from PIL import Image
-
-
-sys.path.append('face-alignment')
-
-from face_aligner import align_image
-from face_aligner import get_detector
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -29,17 +25,12 @@ def is_it_video(path):
 async def send_ready_images(ready_queue, bot):
     while True:
         if ready_queue.qsize():
-            path, chat_id = ready_queue.get()
-            if is_it_video(path):
-                print('sending video to {}'.format(chat_id))
-                with open(path, 'rb') as f:
-                    await bot.send_video(chat_id=chat_id, video=f)
-            else:
-                print('sending photo to {}'.format(chat_id))
-                with open(path, 'rb') as f:
-                    await bot.send_photo(chat_id=chat_id, photo=f)
+            image_path, chat_id = ready_queue.get()
+            print('sending photo to {}'.format(chat_id))
+            with open(image_path, 'rb') as image:
+                await bot.send_photo(chat_id=chat_id, photo=image)
         else:
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(1)
 
 
 class RecognizeThread(Thread):
@@ -48,105 +39,92 @@ class RecognizeThread(Thread):
         Thread.__init__(self)
         self.task_queue = task_queue
         self.ready_queue = queue.Queue()
-        self.batch_size = 4
-
-        self.landmarks_detector = get_detector()
-
+        self.batch_size = 1
+        self.model = MTCNN(select_largest=True, device=device)
         url1 = "Stylegan3/models/stylegan3-r-ffhq-1024x1024.pkl"
         url2 = "Stylegan3/models/stylegan3-r-ffhq-1024x1024-cartoon.pkl"
         url3 = "Stylegan3/models/vgg16.pkl"
-
         print('LOADING MODELS')
-
         with dnnlib.util.open_url(url1) as fp:
             self.G1 = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device)
         with dnnlib.util.open_url(url2) as fp:
             self.G2 = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device)
-        self.vgg16 = get_feature_detector(url3).eval().to(device)
-
+        with dnnlib.util.open_url(url3) as fp:
+            self.vgg16 = get_feature_detector(url3).eval().to(device)
         print('MODELS LOADED')
 
-    def align_face_on_image(self, image_path):
-        for face in align_image(self.landmarks_detector, image_path):
-            yield face
+    def predict(self, images):
+        res = []
+        for image in images:
+            #             image = (self.model(image) + 1) / 2 * 255
+            #             print(image.max())
+            #             image = image.permute(1, 2, 0).numpy().astype(np.uint8)
 
-    def predict(self, image_paths):
-        for image_path in image_paths:
-            for face in self.align_face_on_image(image_path):
-                yield image_path, predict.predict_by_noise(self.G1, self.G2, self.vgg16, face, 0, 10)
+            res.append(predict.predict_by_noise(self.G1, self.G2, self.vgg16, image, 0, 500))
+
+        return res
 
     def get_waiting_time(self):
         return round(self.task_queue.qsize() * 10 / 60, 2)
 
-    def predict_video(self, video_path):
+    def predict_batch(self, batch, res):
+        pred = self.predict(np.array(batch))
+        res = np.stack([pred, res]) if res.size() else pred
+
+    def predict_video(self, video_path, chat_id):
         cap = cv2.VideoCapture(video_path)
 
-        save_path = video_path[:video_path.rfind('.')] + '_anim.mp4'
-        frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        save_path = 'res_' + video_path[:video_path.rfind('.')] + '.mp4'
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         fps = cap.get(cv2.CAP_PROP_FPS)
-
-        freq = fps // 10
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-        out = cv2.VideoWriter(save_path, fourcc, 10, (1024, 1024))
+        out = cv2.VideoWriter(save_path, fourcc, fps, (width, height))
 
         batch = []
-        cnt = 0
+        res = np.array([])
         while cap.isOpened():
             success, image = cap.read()
             if not success:
                 break
 
-            cnt += 1
-            if cnt % freq != 0:
-                continue
-
-            batch.append(Image.fromarray(image))
+            batch.append(image)
             if len(batch) == self.batch_size:
-                for image in self.predict(batch):
-                    print(np.array(image).shape)
-                    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-                    out.write(image)
-
+                self.predict_batch(batch, res)
                 batch = []
 
+            k = cv2.waitKey(25)
+            if k == 27:
+                break
         if len(batch):
-            for image in self.predict(np.array(batch)):
-                out.write(cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
+            self.predict_batch(batch, res)
+
+        for image in res:
+            out.write(image)
 
         out.release()
         cap.release()
-        print('video released')
-        return save_path
 
     def run(self) -> None:
-        image_path_counter = {}
-
         while True:
             cnt = 0
-
             chats = []
             image_paths = []
-
+            images = []
             while cnt < self.batch_size and self.task_queue.qsize():
                 image_path, chat_id = self.task_queue.get()
-
                 if is_it_video(image_path):
-                    path = self.predict_video(image_path)
-                    self.ready_queue.put((path, chat_id))
+                    # self.predict_video(image_path, chat_id)
                     continue
                 else:
                     image_paths.append(image_path)
                     chats.append(chat_id)
-
+                    image = Image.open(image_path).convert('RGB')
+                    images.append(image)
                     cnt += 1
 
-            for i, (image_path, image) in enumerate(self.predict(image_paths)):
-                if image_path in image_path_counter:
-                    image_path_counter[image_path] += 1
-                else:
-                    image_path_counter[image_path] = 0
-
-                image_path = image_path[:image_path.rfind('.')] + f'{image_path_counter[image_path]:03d}.' + '.png'
-                image.save(image_path)
-                self.ready_queue.put((image_path, chats[i]))
+            pred = self.predict(images)
+            for i, image in enumerate(pred):
+                image.save(image_paths[i])
+                self.ready_queue.put((image_paths[i], chats[i]))
